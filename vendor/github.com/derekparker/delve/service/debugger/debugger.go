6fe57ed0e5eb1aec58fe8c6c20a67e923bb6372c
a <a@kra>
2017-02-28 07:14:44 +0100

    update delve

--- vendor/github.com/derekparker/delve/service/debugger/debugger.go
+++ vendor/github.com/derekparker/delve/service/debugger/debugger.go
@@ -750,7 +750,7 @@ func (d *Debugger) convertStacktrace(rawlocs []proc.Stackframe, cfg *proc.LoadCo
 	locations := make([]api.Stackframe, 0, len(rawlocs))
 	for i := range rawlocs {
 		frame := api.Stackframe{Location: api.ConvertLocation(rawlocs[i].Call)}
-		if cfg != nil {
+		if cfg != nil && rawlocs[i].Current.Fn != nil {
 			var err error
 			scope := rawlocs[i].Scope(d.target.CurrentThread())
 			locals, err := scope.LocalVariables(*cfg)
